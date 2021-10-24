ed76fe86d170ee983810d2f702195951a980731a
Stuti Singhal <78038262+StutiSinghal12@users.noreply.github.com>
2021-10-24 16:07:53 +0530

    Create FACEBOOK2.sql

---
+++ FACEBOOK2.sql
@@ -0,0 +1,16 @@
+Rank Variance Per Country
+Which countries have risen in the rankings based on the number of 
+comments between Dec 2019 vs Jan 2020? Hint: Avoid gaps between ranks when ranking countries.
+
+FB_comments_count
+user_id                 int
+created_At              date time
+number_of_comments     int
+
+fb_active_users
+user_id                int
+name                   varchar
+status                 varchar
+country                varchar
+
+
