0a4d2fd75658110dfddcff8020a324968da7d202
Stuti Singhal <78038262+StutiSinghal12@users.noreply.github.com>
2022-07-03 19:27:21 +0530

    Update execution_order.sql

--- execution_order.sql
+++ execution_order.sql
@@ -1,10 +1,13 @@
 parsing----> syntax check,semantic check,
              execution :-
              from
+             join
              where
              groupby
              having
              select
              distinct
              order by
+FJWG HSDO
              
+>>distinct and order by doesn't work together 
