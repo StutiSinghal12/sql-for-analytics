51693e74dbb43c8881d4400f53a6bb28391af77c
Stuti Singhal <78038262+StutiSinghal12@users.noreply.github.com>
2022-07-03 19:07:50 +0530

    Create execution_order.sql

---
+++ execution_order.sql
@@ -0,0 +1,10 @@
+parsing----> syntax check,semantic check,
+             execution :-
+             from
+             where
+             groupby
+             having
+             select
+             distinct
+             order by
+             
