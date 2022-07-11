2f5ee85d500278e08ca591d3c311fc882d88cb47
Stuti Singhal <78038262+StutiSinghal12@users.noreply.github.com>
2022-07-11 12:44:53 +0530

    Update cx_retention.sql

--- cx_retention.sql
+++ cx_retention.sql
@@ -1,5 +1,13 @@
+---retension 
 select month(this_month.order_date),count(last_month.order_id)
 from transa as this month left join trans as last month 
 where datediff(month,this_month.order_date,last_month.order_date)=1
 ad this_month.cx_id=last_month.cx_id
 group by month(this_month.order_date)
+
+--churn 
+select month(last_month.order_date),count(last_month.order_id)
+from transa as last month left join trans as this month 
+where datediff(month,last_month.order_date,this_month.order_date)=1
+ad this_month.cx_id=last_month.cx_id and this_mnth.customer_id is null
+group by month(last_month.order_date)
